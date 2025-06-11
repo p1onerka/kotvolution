e75aad5361d830e3486cbc5e88aeeaeab9fb8914
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-06-11 13:02:19 +0300

    feat: io
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

---
+++ src/main/kotlin/model/io.kt
@@ -0,0 +1,16 @@
+package model
+
+import org.bytedeco.opencv.global.opencv_imgcodecs
+import org.bytedeco.opencv.global.opencv_imgproc
+import org.bytedeco.opencv.opencv_core.Mat
+
+fun uploadPic(path: String): Mat {
+    val image = opencv_imgcodecs.imread(path)
+    val grayScale = Mat()
+    opencv_imgproc.cvtColor(image, grayScale, opencv_imgproc.COLOR_BGR2GRAY)
+    return grayScale
+}
+
+fun downloadPic(path: String, input: Mat) {
+    opencv_imgcodecs.imwrite(path, input)
+}
