cbc63af49484cee541628c7683fa4e1fa836a0d4
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-06-11 13:03:33 +0300

    feat: fst main
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,63 @@
+/* there will be GUI if I'm not too lazy */
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+import model.*
+//import view.mainScreen
+import viewModel.MainViewModel
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
+
+fun main(args: Array<String>) = application {
+    /*Window(onCloseRequest = ::exitApplication) {
+        App()
+    }*/
+    /*val inputPath = if (args.isEmpty()) { "src/main/resources/happybd.jpeg" } else args[0]
+    val input = uploadPic(inputPath)
+    val testF = arrayOf(
+    doubleArrayOf(0.0, 0.2, 0.0),
+    doubleArrayOf(0.2, 0.2, 0.2),
+    doubleArrayOf(0.0, 0.2, 0.0))
+    val testF2 = arrayOf(
+        doubleArrayOf(1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+        doubleArrayOf(0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0))
+    val res = convolute(input, testF2, 1.0/9.0, 0.0)
+    val outputPath = if (args.size < 2) { "src/main/resources/happybd_out.jpeg" } else args[1]
+    downloadPic(outputPath, res)*/
+    val viewModel = MainViewModel()
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Image Convolution App"
+    ) {
+        //mainScreen(viewModel, this.window)
+    }
+}
