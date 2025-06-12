ad726718ebd3838101f1eb1389e78ae578f1bdfb
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-06-13 00:38:47 +0300

    ref: exclude grayscale conversion from input
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- src/main/kotlin/model/io.kt
+++ src/main/kotlin/model/io.kt
@@ -6,9 +6,10 @@ import org.bytedeco.opencv.opencv_core.Mat
 
 fun uploadPic(path: String): Mat {
     val image = opencv_imgcodecs.imread(path)
-    val grayScale = Mat()
-    opencv_imgproc.cvtColor(image, grayScale, opencv_imgproc.COLOR_BGR2GRAY)
-    return grayScale
+    //val grayScale = Mat()
+    //opencv_imgproc.cvtColor(image, grayScale, opencv_imgproc.COLOR_BGR2GRAY)
+    //return grayScale
+    return image
 }
 
 fun downloadPic(path: String, input: Mat) {
