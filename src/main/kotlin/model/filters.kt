96eb8ba9e75151ad766ee2ab5ea0f10d57c10cff
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-06-12 20:13:05 +0300

    ref: change val to const val in filters
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- src/main/kotlin/model/filters.kt
+++ src/main/kotlin/model/filters.kt
@@ -4,15 +4,15 @@ val blur = arrayOf(
     doubleArrayOf(0.0, 0.2, 0.0),
     doubleArrayOf(0.2, 0.2, 0.2),
     doubleArrayOf(0.0, 0.2, 0.0))
-val factorBlur = 1.0
-val biasBlur = 0.0
+const val factorBlur = 1.0
+const val biasBlur = 0.0
 
 val id = arrayOf(
     doubleArrayOf(0.0, 0.0, 0.0),
     doubleArrayOf(0.0, 1.0, 0.0),
     doubleArrayOf(0.0, 0.0, 0.0))
-val factorId = 1.0
-val biasId = 0.0
+const val factorId = 1.0
+const val biasId = 0.0
 
 val motion = arrayOf(
     doubleArrayOf(1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
@@ -24,8 +24,8 @@ val motion = arrayOf(
     doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0),
     doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0),
     doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0))
-val factorMotion = 1.0/9.0
-val biasMotion = 0.0
+const val factorMotion = 1.0/9.0
+const val biasMotion = 0.0
 
 val horEdges = arrayOf(
     doubleArrayOf(0.0, 0.0, -1.0, 0.0, 0.0),
@@ -33,8 +33,8 @@ val horEdges = arrayOf(
     doubleArrayOf(0.0, 0.0, 2.0, 0.0, 0.0),
     doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0),
     doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0))
-val factorHorEdges = 1.0
-val biasHorEdges = 0.0
+const val factorHorEdges = 1.0
+const val biasHorEdges = 0.0
 
 val verEdges = arrayOf(
     doubleArrayOf(0.0, 0.0, -1.0, 0.0, 0.0),
@@ -42,12 +42,12 @@ val verEdges = arrayOf(
     doubleArrayOf(0.0, 0.0, 4.0, 0.0, 0.0),
     doubleArrayOf(0.0, 0.0, -1.0, 0.0, 0.0),
     doubleArrayOf(0.0, 0.0, -1.0, 0.0, 0.0))
-val factorVerEdges = 1.0
-val biasVerEdges = 0.0
+const val factorVerEdges = 1.0
+const val biasVerEdges = 0.0
 
 val sharpen = arrayOf(
     doubleArrayOf(-1.0, -1.0, -1.0),
     doubleArrayOf(-1.0, 9.0, -1.0),
     doubleArrayOf(-1.0, -1.0, -1.0))
-val factorSharpen = 1.0
-val biasSharpen = 0.0
+const val factorSharpen = 1.0
+const val biasSharpen = 0.0
