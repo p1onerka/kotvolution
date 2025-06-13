8883226c5a1191be3a307a8a9a781b496a28e50e
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-06-13 12:17:11 +0300

    feat: parallel with arbitrary frame
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- src/main/kotlin/model/parallel/withCoroutines.kt
+++ src/main/kotlin/model/parallel/withCoroutines.kt
@@ -11,8 +11,8 @@ fun convoluteAsyncY(pic: Mat, filter: Array<DoubleArray>, factor: Double, bias: 
     val res = pic.clone()
     val filterSize = filter.size
 
-    val jobs = List(h) { y ->
-        async(Dispatchers.Default) {
+    for (y in 0 until h) {
+        launch(Dispatchers.Default) {
             for (x in 0 until w) {
                 var blue = 0.0
                 var green = 0.0
@@ -39,8 +39,6 @@ fun convoluteAsyncY(pic: Mat, filter: Array<DoubleArray>, factor: Double, bias: 
             }
         }
     }
-
-    jobs.awaitAll()
     return@runBlocking res
 }
 
@@ -50,8 +48,8 @@ fun convoluteAsyncX(pic: Mat, filter: Array<DoubleArray>, factor: Double, bias: 
     val res = pic.clone()
     val filterSize = filter.size
 
-    val jobs = List(w) { x ->
-        async(Dispatchers.Default) {
+    for (x in 0 until w) {
+        launch(Dispatchers.Default) {
             for (y in 0 until h) {
                 var blue = 0.0
                 var green = 0.0
@@ -79,7 +77,6 @@ fun convoluteAsyncX(pic: Mat, filter: Array<DoubleArray>, factor: Double, bias: 
         }
     }
 
-    jobs.awaitAll()
     return@runBlocking res
 }
 
@@ -88,11 +85,10 @@ fun convoluteAsyncPixel (pic: Mat, filter: Array<DoubleArray>, factor: Double, b
     val h = pic.rows()
     val res = pic.clone()
     val filterSize = filter.size
-    val jobs = mutableListOf<Deferred<Unit>>()
 
     for (y in 0 until h) {
         for (x in 0 until w) {
-            val job: Deferred<Unit> = async(Dispatchers.Default) {
+            launch(Dispatchers.Default) {
                 var blue = 0.0
                 var green = 0.0
                 var red = 0.0
@@ -116,10 +112,53 @@ fun convoluteAsyncPixel (pic: Mat, filter: Array<DoubleArray>, factor: Double, b
                 res.ptr(y, x).put(1L, g)
                 res.ptr(y, x).put(2L, r)
             }
-            jobs += job
         }
     }
+    return@runBlocking res
+}
 
-    jobs.awaitAll()
+fun convoluteAsyncFrame (pic: Mat, filter: Array<DoubleArray>, factor: Double, bias: Double, frameW: Int, frameH: Int): Mat = runBlocking {
+    val w = pic.cols()
+    val h = pic.rows()
+    val res = pic.clone()
+    val filterSize = filter.size
+
+    for (y in 0 until h step frameH) {
+        for (x in 0 until w step frameW) {
+            val endFrameX = min(x+frameW, w)
+            val endFrameY = min(y+frameH, h)
+            launch(Dispatchers.Default) {
+                for (frameY in y until endFrameY) {
+                    for (frameX in x until endFrameX) {
+                        var blue = 0.0
+                        var green = 0.0
+                        var red = 0.0
+
+                        for (filterY in 0 until filterSize) {
+                            for (filterX in 0 until filterSize) {
+                                val imageX = (frameX - filterSize / 2 + filterX + w) % w
+                                val imageY = (frameY - filterSize / 2 + filterY + h) % h
+
+                                blue += (pic.ptr(imageY, imageX).get(0)
+                                    .toInt() and 0xFF).toDouble() * filter[filterY][filterX]
+                                green += (pic.ptr(imageY, imageX).get(1)
+                                    .toInt() and 0xFF).toDouble() * filter[filterY][filterX]
+                                red += (pic.ptr(imageY, imageX).get(2)
+                                    .toInt() and 0xFF).toDouble() * filter[filterY][filterX]
+                            }
+                        }
+
+                        val b = min(255.0, max(0.0, (factor * blue + bias))).toInt().toByte()
+                        val g = min(255.0, max(0.0, (factor * green + bias))).toInt().toByte()
+                        val r = min(255.0, max(0.0, (factor * red + bias))).toInt().toByte()
+
+                        res.ptr(frameY, frameX).put(0L, b)
+                        res.ptr(frameY, frameX).put(1L, g)
+                        res.ptr(frameY, frameX).put(2L, r)
+                    }
+                }
+            }
+        }
+    }
     return@runBlocking res
 }
